e3774ca482af354ae0a6a7288855dc0bc27fcce6
NARodman <ninaalanier8@gmail.com>
2025-10-02 18:58:45 -0400

    Update: enable Swagger

--- FinanceTracker.Api/Program.cs
+++ FinanceTracker.Api/Program.cs
@@ -1,26 +1,37 @@
 using Microsoft.OpenApi.Models;
-
-
+using System.IO;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container
+
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    var xmlFilename = $"{System.Reflection.Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    c.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+});
 
 var app = builder.Build();
 
-// Configure Swagger for dev environment
+
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwagger(c =>
+    {
+        c.RouteTemplate = "api/{documentName}/swagger.json";
+    });
+
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/api/v1/swagger.json", "Finance Tracker API V1");
+        c.RoutePrefix = "api/docs";
+    });
 }
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
 
-app.MapControllers(); // <-- this maps your FinanceController
-app.Run();
+app.MapControllers(); 
 
+app.Run();
