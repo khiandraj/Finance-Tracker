16368da1aab57c11a0a6c9ffddc019446b2dd404
NARodman <ninaalanier8@gmail.com>
2025-10-08 14:13:03 -0400

    feat(auth): extend User model with Password property

--- FinanceTracker.Api/Controllers/FinanceController.cs
+++ FinanceTracker.Api/Controllers/FinanceController.cs
@@ -72,5 +72,6 @@ namespace FinanceTracker.Api.Controllers
     {
         public int Id { get; set; }
         public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
     }
 }
