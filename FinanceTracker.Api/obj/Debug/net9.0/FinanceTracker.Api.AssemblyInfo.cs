576a9585c73d26501ec4843305a18b451761b430
NARodman <ninaalanier8@gmail.com>
2025-11-24 13:53:07 -0500

    feat(balance): implement BalanceService with CRUD and balance updates

--- FinanceTracker.Api/obj/Debug/net9.0/FinanceTracker.Api.AssemblyInfo.cs
+++ FinanceTracker.Api/obj/Debug/net9.0/FinanceTracker.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinanceTracker.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6bf23d5fe757867d261fb28e0f91bd1da52707e2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ad415cbd59331c6e09166f79dd4dba5f00720bc")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinanceTracker.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinanceTracker.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
